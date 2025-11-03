44dd234a44af4e6bf6257c1f6fa330e5d9e98a3e
ahmatfauzy <ahmatf703@gmail.com>
2025-11-03 21:17:49 +0700

    update schema db

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { register, login } from "../controllers/AuthController";
+import { login } from "../controllers/AuthController";
 
 const router = express.Router();
 
-// router.post("/register", register);
 router.post("/login", login);
 
 export default router;
